1b1a2e9284fc2b1c5b4228d704ecd710c01005af
Andrew Nikolin <877163+AndrewNikolin@users.noreply.github.com>
2024-11-18 12:19:35 -0500

    Add repro for CS8618 suppression issue

---
+++ Cs8618Repro/Test1.cs
@@ -0,0 +1,17 @@
+﻿namespace Cs8618Repro;
+
+[TestClass]
+public sealed class Test1
+{
+    private TestClass _testClass;
+    
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
+
+public class TestClass
+{
+    
+}
