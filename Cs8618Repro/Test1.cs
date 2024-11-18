64bc9f0b5bd23d86d808f0a9d131df2dee2f4019
Andrew Nikolin <877163+AndrewNikolin@users.noreply.github.com>
2024-11-18 12:27:07 -0500

    Add demo init

--- Cs8618Repro/Test1.cs
+++ Cs8618Repro/Test1.cs
@@ -4,6 +4,14 @@
 public sealed class Test1
 {
     private TestClass _testClass;
+    private TestClass _testClassProp { get; set; }
+
+    [TestInitialize]
+    public void Init()
+    {
+        _testClass = new TestClass();
+        _testClassProp = new TestClass();
+    }
     
     [TestMethod]
     public void TestMethod1()
